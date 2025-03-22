de5017bce24e67e3716b4fb6e0e940a164981ff6
thabie05 <105032791+thabie05@users.noreply.github.com>
2025-03-22 14:17:00 +0200

    pushing my work to github

--- Finance_App/src/components/TotalAmount.jsx
+++ Finance_App/src/components/TotalAmount.jsx
@@ -12,7 +12,7 @@ const TotalAmount = ({transactions}) => {
   return (
     <div className="p-4 bg-gray-100 rounded-lg mb-6 w-sm text-center absolute top-50 right-20 left-20">
       <h3 className="text-2xl font-bold text-red-500 mb-80">
-        Total Amount: R{Total}</h3>
+        Total Amount: -R{Total}</h3>
     </div>
   )
 }
