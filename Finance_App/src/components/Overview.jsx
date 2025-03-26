ceee4aec9e32cef9d4bd98e69117aa425fe17ba6
thabie05 <105032791+thabie05@users.noreply.github.com>
2025-03-26 16:41:44 +0200

    added a react-chartjs-2 library so that i can add a chart in the overview component

--- Finance_App/src/components/Overview.jsx
+++ Finance_App/src/components/Overview.jsx
@@ -1,11 +1,33 @@
-import React from 'react'
+import TotalAmount from './TotalAmount'
+import { useEffect, useState } from 'react';
 
 const Overview = () => {
+ 
+    const [incomeTransactions, setIncomeTransactions] = useState([]);
+    const [expenseTransactions, setExpenseTransactions] = useState([]);
+
+    
+       // Load transactions from local storage on component mount
+       useEffect(() => {
+        const storedIncome = localStorage.getItem("moneyIn");
+        const storedExpenses = localStorage.getItem("moneyOut");
+        
+        if (storedIncome) setIncomeTransactions(JSON.parse(storedIncome));
+        if (storedExpenses) setExpenseTransactions(JSON.parse(storedExpenses));
+      }, []);
+
+      
+
   return (
     <div>
-      <h1 className='text-3xl font-bold underline text-center'>Overview</h1>
+      <h1 className='text-3xl font-bold text-center'>Overview</h1>
+      <TotalAmount 
+        incomeTransactions={incomeTransactions}
+        expenseTransactions={expenseTransactions}
+      />
+      <p className='text-center'>length of incomeTransactions: {incomeTransactions.length}</p>
     </div>
   )
 }
 
-export default Overview
+export default Overview;
--- Finance_App/src/components/TotalAmount.jsx
+++ Finance_App/src/components/TotalAmount.jsx
@@ -1,3 +1,9 @@
+
+import { Chart as ChartJS, ArcElement, Tooltip, Legend } from 'chart.js';
+import { Doughnut } from 'react-chartjs-2';
+ChartJS.register(ArcElement, Tooltip, Legend);
+
+
 const TotalAmount = ({ incomeTransactions, expenseTransactions }) => {
 
   const totalIncome = incomeTransactions.reduce((sum, transaction) => {
@@ -10,8 +16,29 @@ const TotalAmount = ({ incomeTransactions, expenseTransactions }) => {
 
   const totalSaved = totalIncome - totalExpense;
 
+  const data = {
+    labels: ['Total Income', 'Total Expense', 'Total Saved'],
+    datasets: [
+      {
+        label: '# of Votes',
+        data: [totalIncome, totalExpense, totalSaved],
+        backgroundColor: [
+          '#00ff40',
+          '#ff0000',
+          '#0000ff',
+        ],
+        borderColor: [
+          '#00ff40',
+          '#ff0000',
+          '#0000ff',
+        ],
+        borderWidth: 1,
+      },
+    ],
+  };
+
   return (
-    <div className="totalAmount  ml-12 rounded-3xl shadow-lg mt-40 min-w-sm text-center p-10 gap-20 flex flex-col max-h-110 ">
+    <div className=" totalAmount  ml-12 rounded-3xl shadow-lg mt-40 min-w-sm text-center p-10 gap-20 flex max-h-110 ">
       <div className="bg-linear-to-r from-[#434e55af] to-[#000000bd] absolute w-76 h-110 rounded-3xl bottom-76"></div>
       <div className="z-1 flex flex-col gap-20">
       <h3 className="text-2xl font-bold text-green-500">
@@ -23,9 +50,11 @@ const TotalAmount = ({ incomeTransactions, expenseTransactions }) => {
       <h3 className="text-2xl font-bold text-blue-300">
         Total Saved:<br/> R{totalSaved.toFixed(2)}
       </h3>
+      
+      
       </div>
       
-      
+      <Doughnut className='w-90 h-90 mx-auto' data={data} />
     </div>
   );
 };
