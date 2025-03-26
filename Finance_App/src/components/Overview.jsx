243fba6ee91b83aad76a71ede662acedd41e1f96
thabie05 <105032791+thabie05@users.noreply.github.com>
2025-03-26 18:01:00 +0200

    removed the chart from total amount

--- Finance_App/src/components/Overview.jsx
+++ Finance_App/src/components/Overview.jsx
@@ -1,5 +1,10 @@
-import TotalAmount from './TotalAmount'
 import { useEffect, useState } from 'react';
+import { Chart as ChartJS, ArcElement, Tooltip, Legend } from 'chart.js';
+import { Doughnut } from 'react-chartjs-2';
+ChartJS.register(ArcElement, Tooltip, Legend);
+
+
+
 
 const Overview = () => {
  
@@ -16,16 +21,42 @@ const Overview = () => {
         if (storedExpenses) setExpenseTransactions(JSON.parse(storedExpenses));
       }, []);
 
+      const totalIncome = incomeTransactions.reduce((sum, transaction) => {
+        return sum + (parseFloat(transaction.amount) || 0);
+      }, 0);
+    
+      const totalExpense = expenseTransactions.reduce((sum, transaction) => {
+        return sum + (parseFloat(transaction.amount) || 0);
+      }, 0);
+    
+      const totalSaved = totalIncome - totalExpense;
       
+        const data = {
+          labels: ['Total Income', 'Total Expense', 'Total Saved'],
+          datasets: [
+            {
+              label: '# of Votes',
+              data: [totalIncome, totalExpense, totalSaved],
+              backgroundColor: [
+                '#00ff40',
+                '#ff0000',
+                '#0000ff',
+              ],
+              borderColor: [
+                '#00ff40',
+                '#ff0000',
+                '#0000ff',
+              ],
+              borderWidth: 1,
+            },
+          ],
+        };
 
   return (
     <div>
       <h1 className='text-3xl font-bold text-center'>Overview</h1>
-      <TotalAmount 
-        incomeTransactions={incomeTransactions}
-        expenseTransactions={expenseTransactions}
-      />
-      <p className='text-center'>length of incomeTransactions: {incomeTransactions.length}</p>
+      <Doughnut className='w-90 h-90 mx-auto' data={data} />
+      <p className='text-center'>length of incomeTransactions: {totalIncome}</p>
     </div>
   )
 }
