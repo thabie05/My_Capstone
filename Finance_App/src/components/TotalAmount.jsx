6005c5701f93d847f56e45ed11e20e9f9fb86586
thabie05 <105032791+thabie05@users.noreply.github.com>
2025-04-05 08:41:37 +0200

    enhance styling in TotalAmount component by adding padding to income, expense, and saved totals

--- Finance_App/src/components/TotalAmount.jsx
+++ Finance_App/src/components/TotalAmount.jsx
@@ -27,15 +27,15 @@ const TotalAmount = ({ incomeTransactions, expenseTransactions, selectedDate }) 
     <div className="totalAmount p-4 w-full">
       <div className="bg-gradient-to-r from-[#434e55af] to-[#000000bd] w-full max-w-sm mx-auto rounded-3xl shadow-lg p-6">
       <div className="flex flex-col items-center justify-center gap-4 text-center">
-      <h3 className="text-xl font-bold md:text-2xl text-green-500">
+      <h3 className="text-xl font-bold md:text-2xl py-4 text-green-500">
         Total Income:<br/> R{filteredIncome.toFixed(2)}
       </h3>
-      <hr className="w-3/4 border-gray-600 my-1" />
-      <h3 className="text-xl md:text-2xl font-bold text-red-500">
+      <hr className="w-3/4 border-gray-600 my-1 " />
+      <h3 className="text-xl py-4 md:text-2xl font-bold text-red-500">
         Total Expense:<br/> R{filteredExpense.toFixed(2)}
       </h3>
       <hr className="w-3/4 border-gray-600 my-1" />
-      <h3 className="text-xl md:text-2xl font-bold pb-8 text-blue-300">
+      <h3 className="text-xl md:text-2xl font-bold py-4 text-blue-300">
         Total Saved:<br/> <span className={totalSaved >= 0 ? 'text-blue-300' : 'text-orange-400'}>
               R{totalSaved.toFixed(2)}
             </span>
